4f8fbec54ef4e597575878299955b8f35983a5e0
shalini <shalinia.iitd@gmail.com>
2025-01-21 12:26:42 +0530

    myntra etc

---
+++ PysparkAPI/session.py
@@ -0,0 +1,11 @@
+# from pyspark.sql.session import SparkSession
+# import pandas as pd
+#
+# df_p = pd.read_csv('data.csv')
+#
+# spark = SparkSession.builder.appName("Practise").getOrCreate()
+# df_s = spark.read.csv('data.csv')
+
+
+
+
